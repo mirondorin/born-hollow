87006abed916b5c9058015f22e91dda7b37ea698
Dorin Miron <38944987+mirondorin@users.noreply.github.com>
2024-12-23 18:29:15 +0200

    Feature: logger addon (#5)
    * Add logger
    
    * Start multiplayer template
    
    * Remove unused scripts/scenes

--- source/main.gd
+++ source/main.gd
@@ -1,11 +1,17 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func start_game() -> void:
+	Lobby.load_game("")
+
+
+func _on_host_button_pressed() -> void:
+	Lobby.create_game()
+
+
+func _on_join_button_pressed() -> void:
+	Lobby.join_game()
